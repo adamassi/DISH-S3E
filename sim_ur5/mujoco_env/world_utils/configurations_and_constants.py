f5816fb70c060cc772baaf0229b8523b57c98dad
adamassi <adamassi@campus.technion.ac.il>
2025-06-19 20:55:49 +0300

    Add wooden fork and wooden spoon objects to Mujoco environment
    - Created XML definition for a wooden fork object, including mesh and texture assets.
    - Added texture file for the wooden fork.
    - Updated configurations to include multiple wooden spoon and wooden fork instances with unique positions and joints.
    - Enhanced ObjectManager to recognize new objects and their joint names.
    - Implemented unit tests to verify space availability in the dishwasher with new objects.

--- sim_ur5/mujoco_env/world_utils/configurations_and_constants.py
+++ sim_ur5/mujoco_env/world_utils/configurations_and_constants.py
@@ -15,6 +15,18 @@ sceneHouseTableWorld = SceneSpec(
         ObjectSpec('dish_can', base_pos=[0.0, 0.6, 0.4], base_joints=(JointSpec('free',attrs={'name': 'dish4_fj'}),),),  # Can object with a free joint
         ObjectSpec('Dishwasher', base_pos=[0.6, -1, 0.]),  # Dishwasher object
         ObjectSpec('plate', base_pos=[0.6, -0.421, 0.57], base_joints=(JointSpec('free',attrs={'name': 'dish6_fj'}),), base_rot=[1.57079632679, 0, 0]),
+        ObjectSpec('wood_spoon', base_pos=[-0.6, -0.6, 0.03], base_joints=(JointSpec('free',attrs={'name': 'dish7_fj'}),),),  # Wooden spoon object
+        ObjectSpec('wood_spoon', base_pos=[-0.6, -0.6, 0.2], base_joints=(JointSpec('free',attrs={'name': 'dish8_fj'}),),),  # Wooden spoon object
+        ObjectSpec('wood_spoon', base_pos=[-0.6, -0.6, 0.1], base_joints=(JointSpec('free',attrs={'name': 'dish9_fj'}),),),  # Wooden spoon object
+        ObjectSpec('wood_spoon', base_pos=[-0.6, -0.6, 0.23], base_joints=(JointSpec('free',attrs={'name': 'dish10_fj'}),),),  # Wooden spoon object
+        ObjectSpec('wooden_fork', base_pos=[0.7, 0.6, 0.05], base_joints=(JointSpec('free',attrs={'name': 'dish11_fj'}),),),  # Wooden fork object
+        ObjectSpec('wooden_fork', base_pos=[0.8, 0.6, 0.25], base_joints=(JointSpec('free',attrs={'name': 'dish12_fj'}),),),  # Wooden fork object
+        ObjectSpec('knife', base_pos=[0.7, 0.6, 0.15], base_joints=(JointSpec('free',attrs={'name': 'dish13_fj'}),),),  # Knife object
+        ObjectSpec('knife', base_pos=[0.8, 0.6, 0.35], base_joints=(JointSpec('free',attrs={'name': 'dish14_fj'}),),),  # Knife object
+
+
+
+        
         
     ),
     render_camera='top-right',  # Camera used for rendering
